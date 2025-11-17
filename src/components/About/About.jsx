d65c395216f4a89071854907e6618b1a4cd539f2
gam7ech <gam7zy@gmail.com>
2025-11-17 17:57:07 +0300

    Обнова

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,5 @@
 import './About.scss';
 import aboutBanner from '../../assets/images/about-banner.webp';
-import aboutAbsImg from '../../assets/images/about-abs-image.webp';
 
 function About() {
     return (
@@ -21,10 +20,6 @@ function About() {
                 </div>
                 <figure className="about-banner">
                     <img src={aboutBanner} width="570" height="570" loading="lazy" alt="Интерьер кафе" className="w-100" />
-                    <div className="abs-img abs-img-1 has-before">
-                        <img src={aboutAbsImg} width="285" height="285" loading="lazy" alt="Музыкант" className="w-100" />
-                    </div>
-                    <div className="abs-img abs-img-2 has-before"></div>
                 </figure>
             </div>
         </section>
