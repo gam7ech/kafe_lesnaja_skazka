9f74eecc322596c3052d60e42e0b31bb3b6505c0
gam7ech <gam7zy@gmail.com>
2025-11-20 19:41:30 +0300

    ProjectUpdate

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,34 +1,46 @@
-// import './Preloader.scss';
+import { useEffect, useState } from 'react';
+import './Preloader.scss';
 
-// function Preloader({ isLoading }) {
-//     return (
-//         <div className={`preloader ${!isLoading ? 'loaded' : ''}`}>
-//             <h1 className="preloader__title">
-//                 <span>К</span>
-//                 <span>а</span>
-//                 <span>ф</span>
-//                 <span>е</span>
-//                 <span>-</span>
-//                 <span>Б</span>
-//                 <span>а</span>
-//                 <span>р</span>
-//                 <span className="preloader__space"></span>
-//                 <span>Л</span>
-//                 <span>е</span>
-//                 <span>с</span>
-//                 <span>н</span>
-//                 <span>а</span>
-//                 <span>я</span>
-//                 <span className="preloader__space"></span>
-//                 <span>С</span>
-//                 <span>к</span>
-//                 <span>а</span>
-//                 <span>з</span>
-//                 <span>к</span>
-//                 <span>а</span>
-//             </h1>
-//         </div>
-//     );
-// }
+function Preloader({ isLoading }) {
+    const [isBot, setIsBot] = useState(false);
 
-// export default Preloader;
+    useEffect(() => {
+        const botPattern = /bot|google|baidu|bing|msn|duckduckgo|slurp|yandex/i;
+        if (botPattern.test(navigator.userAgent)) {
+            setIsBot(true);
+        }
+    }, []);
+
+    if (isBot) return null;
+
+    return (
+        <div className={`preloader ${!isLoading ? 'loaded' : ''}`}>
+            <div className="preloader__title">
+                <span>К</span>
+                <span>а</span>
+                <span>ф</span>
+                <span>е</span>
+                <span>-</span>
+                <span>Б</span>
+                <span>а</span>
+                <span>р</span>
+                <span className="preloader__space"></span>
+                <span>Л</span>
+                <span>е</span>
+                <span>с</span>
+                <span>н</span>
+                <span>а</span>
+                <span>я</span>
+                <span className="preloader__space"></span>
+                <span>С</span>
+                <span>к</span>
+                <span>а</span>
+                <span>з</span>
+                <span>к</span>
+                <span>а</span>
+            </div>
+        </div>
+    );
+}
+
+export default Preloader;
